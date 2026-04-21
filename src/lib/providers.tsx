a7dffa33c851a3f7758682a1a648f5688e8ec8a5
JiHo Jeon <wjswlgh96@naver.com>
2026-04-21 17:51:52 +0900

    fix(#7, #8): provider mouted 패턴 제거

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -4,34 +4,24 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useState } from "react";
 
 export default function Providers({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient({}));
 
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
-
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider
-        attribute={"class"}
-        defaultTheme={"system"}
-        enableSystem
-        disableTransitionOnChange
-      >
-        <SessionProvider>{children}</SessionProvider>
-        {/* Tanstack 디버깅 툴 */}
-        <ReactQueryDevtools initialIsOpen={false} />
-      </ThemeProvider>
+      <SessionProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </ThemeProvider>
+      </SessionProvider>
     </QueryClientProvider>
   );
 }
