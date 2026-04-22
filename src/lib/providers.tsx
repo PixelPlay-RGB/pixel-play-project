853bc35c090356f7159a4bb62447ad4738ecf2b0
JiHo Jeon <wjswlgh96@naver.com>
2026-04-22 12:16:43 +0900

    refactor(#12): next-auth 라이브러리 및 관련 파일 제거

--- src/lib/providers.tsx
+++ src/lib/providers.tsx
@@ -2,7 +2,6 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import { ReactNode, useState } from "react";
 
@@ -11,17 +10,10 @@ export default function Providers({ children }: { children: ReactNode }) {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <SessionProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-          <ReactQueryDevtools initialIsOpen={false} />
-        </ThemeProvider>
-      </SessionProvider>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        {children}
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
