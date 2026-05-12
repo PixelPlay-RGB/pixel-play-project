23eb83c802c851ec7d7a3790cdcf45f194c5bd51
JiHo Jeon <wjswlgh96@naver.com>
2026-05-12 13:58:51 +0900

    fix(#47): DB Migration 추가 및 탭 변경시 Select 값 초기화

--- src/stores/chat-room.ts
+++ src/stores/chat-room.ts
@@ -1,4 +1,4 @@
-import { CHAT_ROOM_SORT_OPTIONS_BY_TAB, DEFAULT_CHAT_ROOM_SORT_OPTION } from "@/constants/chat-room";
+import { DEFAULT_CHAT_ROOM_SORT_OPTION } from "@/constants/chat-room";
 import type { ChatRoomSortOption, ChatRoomTab } from "@/types/chat-room";
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
@@ -17,12 +17,10 @@ export const useChatRoomStore = create<ChatRoomState>()(
       sortOption: DEFAULT_CHAT_ROOM_SORT_OPTION,
       setTabType: (tabType) =>
         set(
-          (state) => ({
+          {
             tabType,
-            sortOption: CHAT_ROOM_SORT_OPTIONS_BY_TAB[tabType].includes(state.sortOption)
-              ? state.sortOption
-              : DEFAULT_CHAT_ROOM_SORT_OPTION,
-          }),
+            sortOption: DEFAULT_CHAT_ROOM_SORT_OPTION,
+          },
           false,
           "chatRoom/setTabType",
         ),
